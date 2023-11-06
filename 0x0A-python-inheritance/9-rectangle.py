a2faa6f1e9a1cc5d3ea575a3433ec7b8f7bd32ae
The-alpha-male <ronnymunene@outlook.com>
2023-11-06 14:37:06 -0800

    a class Square that inherits from Rectangle (9-rectangle.py)

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,23 +1,36 @@
 #!/usr/bin/python3
+"""
+Contains the class BaseGeometry and subclass Rectangle
+"""
 
-"""The module is 9-rectangle"""
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+class BaseGeometry:
+    """A class with public instance methods area and integer_validator"""
+    def area(self):
+        """raises an exception when called"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates that value is an integer greater than 0"""
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
 
 class Rectangle(BaseGeometry):
-    """A subclass of BaseGeometry class"""
-
+    """A representation of a rectangle"""
     def __init__(self, width, height):
-        """initialize private attributes"""
+        """instantiation of the rectangle"""
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
 
     def area(self):
-        """returns area of a rectangle and overrides area() from superclass"""
+        """returns the area of the rectangle"""
         return self.__width * self.__height
-    
+
     def __str__(self):
-        """Returns a human readable string representation"""
-        return str("[Rectangle] {}/{}".format(self.__width, self.__height))
+        """informal string representation of the rectangle"""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
