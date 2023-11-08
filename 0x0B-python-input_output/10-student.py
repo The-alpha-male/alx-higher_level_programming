25a4f1d22351e4b9b6cb744e5bf1d6cae82f7868
The-alpha-male <ronnymunene@outlook.com>
2023-11-07 16:08:46 -0800

    a class Student that defines a student by: (based on 9-student.py)

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -7,6 +7,7 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """Initializes the student"""
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
@@ -14,6 +15,7 @@ class Student:
     def to_json(self, attrs=None):
         """returns a dictionary representation of a Student instance
         with specified attributes"""
+
         if attrs is None:
             return self.__dict__
         new_dict = {}
