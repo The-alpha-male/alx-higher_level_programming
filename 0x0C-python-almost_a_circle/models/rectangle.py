e1d2888c1df72abfc6fc4ca1058353547130826b
The-alpha-male <ronnymunene@outlook.com>
2023-11-13 09:36:40 -0800

    class Rectangle that inherits from Base

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,8 +4,10 @@
 
 from models.base import Base
 
+
 class Rectangle(Base):
     """subclass of superclass Base"""
+
 
 def __init__(self, width, height, x=0, y=0, id=None):
     """Initialize instances for class rectangle"""
@@ -15,10 +17,12 @@ def __init__(self, width, height, x=0, y=0, id=None):
     self.y = y
     super().__init__(id)
 
+
 @property
 def width(self):
     """Retrive attribute"""
     return self.__width
+
 
 @width.setter
 def width(self, value):
@@ -29,19 +33,22 @@ def width(self, value):
         raise ValueError("width should be > 0")
     self.__width = value
 
+
 @property
 def height(self):
     """retive height"""
     return self.__height
 
+
 @height.setter
-def height(self,value):
+def height(self, value):
     """set and validate height"""
     if type(value) is not int:
         raise TypeError("Height should be an integer")
     if value <= 0:
         raise ValueError("Height should be > 0")
     self.__height = value
+
 
 @property
 def x(self):
